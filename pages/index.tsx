c13db245498e5ff85a364f6044f45628cbf5100b
Kedllon Kleber <50781783+kkleber1991@users.noreply.github.com>
2022-08-11 22:03:08 -0300

    Pausando projeto
    Pausando para fazer uma landingpage para um possível cliente

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-const Home: NextPage = () => {
+const Index: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Estudos para projeto particular</title>
-        <meta name="description" content="Estudando NextJs com Chakra-ui" />
-        <link rel="icon" href="/faviconks.png" />
-      </Head>
-
-      <main className={styles.main}>
-       
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/ks.png" alt="Vercel Logo" width={85} height={25} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <>
+      Index
+    </>
   )
 }
 
-export default Home
+export default Index
